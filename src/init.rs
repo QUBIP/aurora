954afb69839224b344ab9ec1cdaf06890609fc3e
Nicola Tuveri <nic.tuv@gmail.com>
2024-09-18 15:25:04 +0300

    Start importing OSSL_PARAM work

--- src/init.rs
+++ src/init.rs
@@ -1,7 +1,13 @@
-pub use crate::bindings::OSSL_DISPATCH;
+use crate::bindings::forbidden;
+use crate::bindings::ossl_param_st;
+use crate::bindings::OSSL_DISPATCH;
+use crate::bindings::OSSL_PROV_PARAM_NAME;
 use crate::named;
+use crate::osslparams::OSSLParam;
 use crate::OpenSSLProvider;
 use libc::{c_int, c_void};
+
+use crate::{PROV_NAME, PROV_VER};
 
 #[cfg(feature = "pretty_env_logger")]
 pub use pretty_env_logger as logger;
@@ -14,6 +20,7 @@ pub struct OSSL_CORE_HANDLE {
 }
 
 #[named]
+#[allow(non_snake_case)]
 #[no_mangle]
 pub extern "C" fn OSSL_provider_init(
     handle: *const OSSL_CORE_HANDLE,
@@ -25,7 +32,7 @@ pub extern "C" fn OSSL_provider_init(
     logger::try_init().expect("Failed initializing logger subsystem");
 
     trace!(target: log_target!(), "Just called a 🦀 Rust function from C!");
-    trace!(target: log_target!(), "This is 🌌 {} v{}", OpenSSLProvider::NAME, OpenSSLProvider::VERSION);
+    trace!(target: log_target!(), "This is 🌌 {} v{}", PROV_NAME, PROV_VER);
 
     let mut prov = Box::new(OpenSSLProvider::new(handle, core_dispatch));
     let ourdispatch = prov.get_provider_dispatch();
@@ -46,4 +53,46 @@ pub unsafe extern "C" fn provider_teardown(vprovctx: *mut c_void) {
     let name = prov.name;
     debug!(target: log_target!(), "Teardown of \"{name}\"");
     info!(target: log_target!(), "🦀 Goodbye!");
+}
+
+#[named]
+pub unsafe extern "C" fn gettable_params(vprovctx: *mut c_void) -> *const ossl_param_st {
+    trace!(target: log_target!(), "{}", "Called!");
+    let prov: &mut OpenSSLProvider<'_> = vprovctx.into();
+    (*prov).get_params_array()
+}
+
+#[named]
+pub unsafe extern "C" fn get_params(vprovctx: *mut c_void, params: *mut ossl_param_st) -> c_int {
+    trace!(target: log_target!(), "{}", "Called!");
+    /* It's important to only cast the pointer, not Box it back up, because otherwise the provctx
+     * object would get dropped at the end of this function (and the compiler wouldn't even warn
+     * us about it, because this code is marked unsafe!). */
+    let prov: &OpenSSLProvider<'_> = vprovctx.into();
+
+    /* Here we build a vec of OSSLParam structs which are type-safe wrappers around pointers to the
+     * actual C structs in the params array... */
+    let mut v: Vec<OSSLParam> = Vec::new();
+    let mut i = 0;
+    loop {
+        let p = params.offset(i);
+        if (*p).key.is_null() {
+            break;
+        } else {
+            match OSSLParam::try_from(p) {
+                Ok(param) => v.push(param),
+                Err(_) => eprintln!("Unimplemented param data type: {:?}", (*p).data_type),
+            }
+        }
+        i += 1;
+    }
+
+    /* ... and then we ignore all that work and call C functions directly to write the name of the
+     * provider to the appropriate C struct, because the ability to do this with the OSSLParam Rust
+     * struct isn't implemented yet. */
+    let p: *mut ossl_param_st = forbidden::OSSL_PARAM_locate(params, OSSL_PROV_PARAM_NAME.as_ptr());
+    if !p.is_null() && forbidden::OSSL_PARAM_set_utf8_ptr(p, (prov).c_prov_name().as_ptr()) == 0 {
+        return 0;
+    }
+    1
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 extern crate log;
 
 use ::function_name::named;
+use std::ffi::{CStr, CString};
+use std::sync::LazyLock;
 
 macro_rules! function_path {
     () => {
@@ -17,10 +19,16 @@ macro_rules! log_target {
 
 mod bindings;
 mod init;
+mod osslparams;
 
-use crate::bindings::dispatch_table_entry;
-use crate::init::OSSL_CORE_HANDLE;
-use bindings::{OSSL_DISPATCH, OSSL_FUNC_PROVIDER_TEARDOWN};
+use bindings::dispatch_table_entry;
+use bindings::ossl_param_st;
+use bindings::{
+    OSSL_DISPATCH, OSSL_FUNC_PROVIDER_GETTABLE_PARAMS, OSSL_FUNC_PROVIDER_GET_PARAMS,
+    OSSL_FUNC_PROVIDER_TEARDOWN, OSSL_PROV_PARAM_NAME,
+};
+use init::OSSL_CORE_HANDLE;
+use osslparams::{OSSLParam, OSSLParamData, Utf8PtrData, OSSL_PARAM_END};
 
 /// This is an abstract representation of one Provider instance.
 /// Remember that a single provider module could be loaded multiple
@@ -37,6 +45,9 @@ pub struct OpenSSLProvider<'a> {
     _handle: *const OSSL_CORE_HANDLE,
     _core_dispatch: *const OSSL_DISPATCH,
     pub name: &'a str,
+    pub version: &'a str,
+    params: Vec<OSSLParam>,
+    param_array_ptr: Option<*mut [ossl_param_st]>,
 }
 
 /// We implement the Drop trait to make it explicit when a provider
@@ -54,16 +65,21 @@ impl<'a> Drop for OpenSSLProvider<'a> {
     }
 }
 
-impl<'a> OpenSSLProvider<'a> {
-    pub const NAME: &'a str = env!("CARGO_PKG_NAME");
-    pub const VERSION: &'a str = env!("CARGO_PKG_VERSION");
+pub static PROV_NAME: &str = env!("CARGO_PKG_NAME");
+pub static PROV_VER: &str = env!("CARGO_PKG_VERSION");
 
+impl<'a> OpenSSLProvider<'a> {
     pub fn new(handle: *const OSSL_CORE_HANDLE, core_dispatch: *const OSSL_DISPATCH) -> Self {
         Self {
             data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
             _handle: handle,
             _core_dispatch: core_dispatch,
-            name: Self::NAME,
+            name: PROV_NAME,
+            version: PROV_VER,
+            param_array_ptr: None,
+            params: vec![OSSLParam::Utf8Ptr(Utf8PtrData::new_null(
+                OSSL_PROV_PARAM_NAME,
+            ))],
         }
     }
 
@@ -71,8 +87,72 @@ impl<'a> OpenSSLProvider<'a> {
     pub fn get_provider_dispatch(&mut self) -> *const OSSL_DISPATCH {
         let ret = Box::new([
             dispatch_table_entry!(OSSL_FUNC_PROVIDER_TEARDOWN, crate::init::provider_teardown),
+            dispatch_table_entry!(
+                OSSL_FUNC_PROVIDER_GETTABLE_PARAMS,
+                crate::init::gettable_params
+            ),
+            dispatch_table_entry!(OSSL_FUNC_PROVIDER_GET_PARAMS, crate::init::get_params),
             OSSL_DISPATCH::END,
         ]);
-        Box::into_raw(ret).cast()
+        ret.as_ptr()
+    }
+
+    fn get_params_array(&mut self) -> *const ossl_param_st {
+        // This is kind of like a poor man's std::sync::Once
+        let raw_ptr = match self.param_array_ptr {
+            Some(raw_ptr) => raw_ptr,
+            None => {
+                let slice = self
+                    .params
+                    .clone()
+                    .into_iter()
+                    .map(|p| (unsafe { *p.get_c_struct() }))
+                    .chain(std::iter::once(OSSL_PARAM_END))
+                    .collect::<Vec<_>>()
+                    .into_boxed_slice();
+                let raw_ptr = Box::into_raw(slice);
+                self.param_array_ptr = Some(raw_ptr);
+                raw_ptr
+            }
+        };
+        raw_ptr.cast()
+    }
+
+    pub fn c_prov_name(&self) -> &CStr {
+        // FIXME: this should be turned into `expect` or removed
+        #[allow(clippy::let_and_return)]
+        static L: LazyLock<CString> = LazyLock::new(|| {
+            let _s = CString::new(crate::PROV_NAME).expect("Error parsing cPROV_NAME");
+            _s
+        });
+        L.as_ref()
+    }
+}
+
+impl<'a> From<*mut core::ffi::c_void> for &mut OpenSSLProvider<'a> {
+    #[named]
+    fn from(vctx: *mut core::ffi::c_void) -> Self {
+        trace!(target: log_target!(), "Called for {}",
+        "impl<'a> From<*mut core::ffi::c_void> for &mut OpenSSLProvider<'a>"
+        );
+        let provp = vctx as *mut OpenSSLProvider;
+        if provp.is_null() {
+            panic!("vctx was null");
+        }
+        unsafe { &mut *provp }
+    }
+}
+
+impl<'a> From<*mut core::ffi::c_void> for &OpenSSLProvider<'a> {
+    #[named]
+    fn from(vctx: *mut core::ffi::c_void) -> Self {
+        trace!(target: log_target!(), "Called for {}",
+        "impl<'a> From<*mut core::ffi::c_void> for &OpenSSLProvider<'a>"
+        );
+        let provp = vctx as *const OpenSSLProvider;
+        if provp.is_null() {
+            panic!("vctx was null");
+        }
+        unsafe { &*provp }
     }
 }
