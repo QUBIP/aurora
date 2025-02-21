dc4215392a011e9ba39bcc7c819fdd1a4dce1219
Nicola Tuveri <nic.tuv@gmail.com>
2025-02-21 21:53:10 +0200

    fix(build.rs): improve error handling for git describe
    - Enhanced error handling in build.rs for the git describe command.
      Replaced expect calls with match to provide more informative error
      messages and avoid panics.
    - Added fallback value for git describe failure in main function.

--- build.rs
+++ build.rs
@@ -8,19 +8,22 @@ fn get_git_describe() -> Result<String, Box<dyn Error>> {
         .expect("Failed to execute git describe");
 
     if !output.status.success() {
-        println!("cargo:warning=Failed to get git describe");
         return Err(format!("git describe failed with status: {}", output.status).into());
     }
 
-    let git_describe = String::from_utf8(output.stdout)
-        .expect("Invalid UTF-8 output")
-        .trim()
-        .to_string();
+    let git_describe = match String::from_utf8(output.stdout) {
+        Ok(str) => str.trim().to_string(),
+        Err(e) => return Err(format!("Invalid UTF-8 output: {e:?}").into()),
+    };
 
     Ok(git_describe)
 }
 
 fn main() {
-    let git_describe = get_git_describe().unwrap();
+    let git_describe = get_git_describe().unwrap_or_else(|e| {
+        println!("cargo:warning=Failed to get git describe");
+        eprintln!("Error was {e:?}");
+        "FAILED_TO_GATHER_GIT_DESCRIBE".to_string()
+    });
     println!("cargo:rustc-env=CARGO_GIT_DESCRIBE={}", git_describe);
 }
