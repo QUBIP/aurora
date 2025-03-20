756460433ae828fa6ae51dad92cba7a787c4f7aa
Nicola Tuveri <nic.tuv@gmail.com>
2025-03-20 11:40:05 +0200

    fix(tests): update core_dispatch initialization
    Updated the `core_dispatch` initialization in `new_provctx_for_testing`
    to use an empty slice reference instead of a null pointer.

--- src/tests.rs
+++ src/tests.rs
@@ -2,7 +2,7 @@ use super::*;
 
 pub fn new_provctx_for_testing<'a>() -> OpenSSLProvider<'a> {
     let handle = std::ptr::null();
-    let core_dispatch = std::ptr::null();
+    let core_dispatch = &[];
 
     return OpenSSLProvider::new(handle, core_dispatch);
 }
