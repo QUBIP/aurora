8d87021a198c877a997eff19406fd98666e1038d
Nicola Tuveri <nic.tuv@gmail.com>
2025-03-26 12:08:05 +0200

    feat(aurora/forge): (disabled) example of how to redefine constants locally for aurora

--- src/forge.rs
+++ src/forge.rs
@@ -1,1 +1,74 @@
 pub use openssl_provider_forge::*;
+
+#[cfg(any())]
+pub(crate) mod bindings {
+    pub(super) const OSSL_FUNC_KEYMGMT_UNUSED: u32 = 42;
+
+    #[allow(dead_code)]
+    pub const OSSL_FUNC_KEYMGMT_EXPORT: u32 = {
+        #[cfg(not(feature = "export"))]
+        {
+            OSSL_FUNC_KEYMGMT_UNUSED
+        }
+        #[cfg(feature = "export")]
+        {
+            openssl_provider_forge::bindings::OSSL_FUNC_KEYMGMT_EXPORT
+        }
+    };
+
+    #[allow(dead_code)]
+    pub const OSSL_FUNC_KEYMGMT_EXPORT_TYPES: u32 = {
+        #[cfg(not(feature = "export"))]
+        {
+            OSSL_FUNC_KEYMGMT_UNUSED
+        }
+        #[cfg(feature = "export")]
+        {
+            openssl_provider_forge::bindings::OSSL_FUNC_KEYMGMT_EXPORT_TYPES
+        }
+    };
+
+    pub use openssl_provider_forge::bindings::*;
+
+    #[cfg(test)]
+    mod tests {
+        use crate::tests::common::OurError;
+
+        struct TestCTX<'a> {
+            //provctx: OpenSSLProvider<'a>,
+            phantom: std::marker::PhantomData<&'a ()>,
+        }
+
+        impl<'a> Default for TestCTX<'a> {
+            fn default() -> Self {
+                Self {
+                    phantom: Default::default(),
+                }
+            }
+        }
+
+        fn setup<'a>() -> Result<TestCTX<'a>, OurError> {
+            crate::tests::common::setup()?;
+
+            //use crate::tests::new_provctx_for_testing;
+            //let provctx = new_provctx_for_testing();
+            //let testctx = TestCTX { provctx };
+
+            let testctx = TestCTX::default();
+
+            Ok(testctx)
+        }
+
+        #[test]
+        fn test_constants() {
+            let testctx = setup().expect("Failed to initialize test setup");
+            //let provctx = testctx.provctx;
+            let _ = testctx;
+
+            assert_eq!(
+                crate::forge::bindings::OSSL_FUNC_KEYMGMT_EXPORT,
+                openssl_provider_forge::bindings::OSSL_FUNC_KEYMGMT_EXPORT
+            )
+        }
+    }
+}
