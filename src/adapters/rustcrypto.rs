a2aceb796b96b219479d60f5d52fb7f765bfd059
Nicola Tuveri <nic.tuv@gmail.com>
2025-10-25 01:40:58 +0300

    cleanup(rustcrypto): remove vestiges of disabled algorithms
    Remove all vestiges of the disabled SLHDSASHAKE192f and SLHDSASHAKE256s
    algorithms from the rustcrypto adapter.
    
    This remove all the related module files.
    
    No associated code remains for these parameter sets within the rustcrypto adapter,
    which, effectively, only supports SLH-DSA-SHAKE-128f.
    
    Signed-off-by: Nicola Tuveri <nic.tuv@gmail.com>
    Change-Id: I6a6a6964d33b7e7a359f09aa46dc03ab4c45c273

--- src/adapters/rustcrypto.rs
+++ src/adapters/rustcrypto.rs
@@ -19,12 +19,6 @@ const PROPERTY_DEFINITION: &CStr = c"x.author='QUBIP',x.qubip.adapter='rustcrypt
 
 #[allow(non_snake_case)]
 pub(crate) mod SLHDSASHAKE128f;
-#[cfg(any())] // module disabled
-#[allow(non_snake_case)]
-pub(crate) mod SLHDSASHAKE192f;
-#[cfg(any())] // module disabled
-#[allow(non_snake_case)]
-pub(crate) mod SLHDSASHAKE256s;
 
 #[derive(Debug)]
 struct RustCryptoAdapter;
@@ -34,19 +28,13 @@ impl AdapterContextTrait for RustCryptoAdapter {
     fn register_algorithms(&self, handle: &mut super::AdaptersHandle) -> Result<(), aurora::Error> {
         trace!(target: log_target!(), "{}", "Called!");
 
-        let signature_algorithms = Box::new([
-            algorithm_to_register!(SLHDSASHAKE128f, SIG_FUNCTIONS),
-            //algorithm_to_register!(SLHDSASHAKE192f, SIG_FUNCTIONS),
-            //algorithm_to_register!(SLHDSASHAKE256s, SIG_FUNCTIONS),
-        ]);
+        let signature_algorithms =
+            Box::new([algorithm_to_register!(SLHDSASHAKE128f, SIG_FUNCTIONS)]);
         // ownership transfers to the iterator which is transferred to the handle
         handle.register_algorithms(OSSL_OP_SIGNATURE, signature_algorithms.into_iter())?;
 
-        let keymgmt_algorithms = Box::new([
-            algorithm_to_register!(SLHDSASHAKE128f, KMGMT_FUNCTIONS),
-            //algorithm_to_register!(SLHDSASHAKE192f, KMGMT_FUNCTIONS),
-            //algorithm_to_register!(SLHDSASHAKE256s, KMGMT_FUNCTIONS),
-        ]);
+        let keymgmt_algorithms =
+            Box::new([algorithm_to_register!(SLHDSASHAKE128f, KMGMT_FUNCTIONS)]);
         // ownership transfers to the iterator which is transferred to the handle
         handle.register_algorithms(OSSL_OP_KEYMGMT, keymgmt_algorithms.into_iter())?;
 
@@ -55,12 +43,6 @@ impl AdapterContextTrait for RustCryptoAdapter {
             // SLHDSASHAKE128f
             decoder_to_register!(SLHDSASHAKE128f, DECODER_DER2SubjectPublicKeyInfo),
             decoder_to_register!(SLHDSASHAKE128f, DECODER_DER2PrivateKeyInfo),
-            // // SLHDSASHAKE192f
-            // decoder_to_register!(SLHDSASHAKE192f, DECODER_DER2SubjectPublicKeyInfo),
-            // decoder_to_register!(SLHDSASHAKE192f, DECODER_DER2PrivateKeyInfo),
-            // // SLHDSASHAKE256s
-            // decoder_to_register!(SLHDSASHAKE256s, DECODER_DER2SubjectPublicKeyInfo),
-            // decoder_to_register!(SLHDSASHAKE256s, DECODER_DER2PrivateKeyInfo),
         ]);
 
         handle.register_algorithms(OSSL_OP_DECODER, decoder_algorithms.into_iter())?;
@@ -71,16 +53,6 @@ impl AdapterContextTrait for RustCryptoAdapter {
             encoder_to_register!(SLHDSASHAKE128f, ENCODER_PrivateKeyInfo2PEM),
             encoder_to_register!(SLHDSASHAKE128f, ENCODER_SubjectPublicKeyInfo2DER),
             encoder_to_register!(SLHDSASHAKE128f, ENCODER_SubjectPublicKeyInfo2PEM),
-            // // SLHDSASHAKE192f
-            // encoder_to_register!(SLHDSASHAKE192f, ENCODER_PrivateKeyInfo2DER),
-            // encoder_to_register!(SLHDSASHAKE192f, ENCODER_PrivateKeyInfo2PEM),
-            // encoder_to_register!(SLHDSASHAKE192f, ENCODER_SubjectPublicKeyInfo2DER),
-            // encoder_to_register!(SLHDSASHAKE192f, ENCODER_SubjectPublicKeyInfo2PEM),
-            // // SLHDSASHAKE256s
-            // encoder_to_register!(SLHDSASHAKE256s, ENCODER_PrivateKeyInfo2DER),
-            // encoder_to_register!(SLHDSASHAKE256s, ENCODER_PrivateKeyInfo2PEM),
-            // encoder_to_register!(SLHDSASHAKE256s, ENCODER_SubjectPublicKeyInfo2DER),
-            // encoder_to_register!(SLHDSASHAKE256s, ENCODER_SubjectPublicKeyInfo2PEM),
         ]);
 
         handle.register_algorithms(OSSL_OP_ENCODER, encoder_algorithms.into_iter())?;
@@ -95,11 +67,7 @@ impl AdapterContextTrait for RustCryptoAdapter {
     ) -> Result<(), aurora::Error> {
         trace!(target: log_target!(), "{}", "Called!");
 
-        let tls_sigalgs = [
-            SLHDSASHAKE128f::capabilities::tls_sigalg::OSSL_PARAM_ARRAY,
-            // SLHDSASHAKE192f::capabilities::tls_sigalg::OSSL_PARAM_ARRAY,
-            // SLHDSASHAKE256s::capabilities::tls_sigalg::OSSL_PARAM_ARRAY,
-        ];
+        let tls_sigalgs = [SLHDSASHAKE128f::capabilities::tls_sigalg::OSSL_PARAM_ARRAY];
         for a in tls_sigalgs {
             let first: &bindings::OSSL_PARAM = a.first().unwrap_or(&CONST_OSSL_PARAM::END);
             let ptr: *const bindings::OSSL_PARAM = std::ptr::from_ref(first);
@@ -112,11 +80,7 @@ impl AdapterContextTrait for RustCryptoAdapter {
     fn register_obj_sigids(&self, handle: &mut super::AdaptersHandle) -> Result<(), aurora::Error> {
         trace!(target: log_target!(), "{}", "Called!");
 
-        let obj_sigids = vec![
-            SLHDSASHAKE128f::OBJ_SIGID,
-            //SLHDSASHAKE192f::OBJ_SIGID,
-            //SLHDSASHAKE256s::OBJ_SIGID
-        ];
+        let obj_sigids = vec![SLHDSASHAKE128f::OBJ_SIGID];
 
         for obj_sigid in obj_sigids {
             handle.register_obj_sigid(obj_sigid)?;
