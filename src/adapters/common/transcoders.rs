8cb94d4da55478859ad2dbe139fa4ceab3356a7e
Nicola Tuveri <nic.tuv@gmail.com>
2025-12-11 16:21:05 +0200

    feat(adapters/common/transcoders): Do not clutter the current namespace when calling `make_pubkey_text_encoder!`
    Signed-off-by: Nicola Tuveri <nic.tuv@gmail.com>
    Change-Id: I6a6a6964001d50feeee0e099d332ab3ce1bd9af8

--- src/adapters/common/transcoders.rs
+++ src/adapters/common/transcoders.rs
@@ -32,6 +32,7 @@ macro_rules! make_pubkey_text_encoder {
         const DISPATCH_TABLE: &'static [$crate::forge::bindings::OSSL_DISPATCH] = {
             mod dispatch_table_module {
                 use super::*;
+                use $crate::adapters::common::helpers::format_hex_bytes;
                 use bindings::{OSSL_FUNC_encoder_does_selection_fn, OSSL_FUNC_ENCODER_DOES_SELECTION};
                 use bindings::{OSSL_FUNC_encoder_encode_fn, OSSL_FUNC_ENCODER_ENCODE};
                 use bindings::{OSSL_FUNC_encoder_freectx_fn, OSSL_FUNC_ENCODER_FREECTX};
@@ -53,82 +54,83 @@ macro_rules! make_pubkey_text_encoder {
                     dispatch_table_entry!(
                         OSSL_FUNC_ENCODER_DOES_SELECTION,
                         OSSL_FUNC_encoder_does_selection_fn,
-                        encoder_functions::does_selection_text
+                        does_selection_text
                     ),
                     dispatch_table_entry!(
                         OSSL_FUNC_ENCODER_ENCODE,
                         OSSL_FUNC_encoder_encode_fn,
-                        encoder_functions::encodeStructurelessToText
+                        encodeStructurelessToText
                     ),
                     $crate::forge::bindings::OSSL_DISPATCH::END,
                 ];
+
+                #[named]
+                pub(super) unsafe extern "C" fn encodeStructurelessToText(
+                    vencoderctx: *mut c_void,
+                    out: *mut $crate::forge::bindings::OSSL_CORE_BIO,
+                    obj_raw: *const c_void,
+                    _obj_abstract: *const $crate::forge::bindings::OSSL_PARAM,
+                    selection: c_int,
+                    _cb: $crate::forge::bindings::OSSL_PASSPHRASE_CALLBACK,
+                    _cbarg: *mut c_void,
+                ) -> c_int {
+                    const SUCCESS: c_int = 1;
+                    const ERROR_RET: c_int = 0;
+                    trace!(target: log_target!(), "{}", "Called!");
+
+                    debug!(target: log_target!(), "Got selection: {selection:#b}");
+                    if (selection & ($crate::forge::bindings::OSSL_KEYMGMT_SELECT_PUBLIC_KEY as c_int)) == 0 {
+                        error!(target: log_target!(), "Invalid selection: {selection:#?}");
+                        return ERROR_RET;
+                    }
+
+                    let encoderctx: &EncoderContext = $crate::handleResult!(vencoderctx.try_into());
+
+                    if obj_raw.is_null() {
+                        error!(target: log_target!(), "No provider-native object passed to encoder");
+                        return ERROR_RET;
+                    }
+
+                    let keypair: &KeyPair = $crate::handleResult!(obj_raw.try_into());
+                    match &keypair.public {
+                        Some(key) => {
+                            let key_bytes = key.encode();
+                            let formatted_key_bytes = format_hex_bytes(15, 4, &key_bytes);
+                            let output = format!("Public key bytes:\n{}\n", formatted_key_bytes);
+                            let output = handleResult!(CString::new(output));
+                            match encoderctx.provctx.BIO_write_ex(out, &output.into_bytes_with_nul()) {
+                                Ok(_bytes_written) => {}
+                                Err(e) => {
+                                    error!(target: log_target!(), "Failure using BIO_write_ex() upcall pointer: {e:?}");
+                                    return ERROR_RET;
+                                }
+                            };
+                            return SUCCESS;
+                        }
+                        None => {
+                            error!(target: log_target!(), "No public key");
+                            return ERROR_RET;
+                        }
+                    }
+                }
+
+                $crate::forge::operations::transcoders::make_does_selection_fn!(
+                    does_selection_text,
+                    $encoder_struct,
+                    ProviderInstance
+                );
             }
 
             dispatch_table_module::TEXT_ENCODER_FUNCTIONS
         };
     }
 
-    #[named]
-    pub(super) unsafe extern "C" fn encodeStructurelessToText(
-        vencoderctx: *mut c_void,
-        out: *mut $crate::forge::bindings::OSSL_CORE_BIO,
-        obj_raw: *const c_void,
-        _obj_abstract: *const $crate::forge::bindings::OSSL_PARAM,
-        selection: c_int,
-        _cb: $crate::forge::bindings::OSSL_PASSPHRASE_CALLBACK,
-        _cbarg: *mut c_void,
-    ) -> c_int {
-        const SUCCESS: c_int = 1;
-        const ERROR_RET: c_int = 0;
-        trace!(target: log_target!(), "{}", "Called!");
-
-        debug!(target: log_target!(), "Got selection: {selection:#b}");
-        if (selection & ($crate::forge::bindings::OSSL_KEYMGMT_SELECT_PUBLIC_KEY as c_int)) == 0 {
-            error!(target: log_target!(), "Invalid selection: {selection:#?}");
-            return ERROR_RET;
-        }
-
-        let encoderctx: &EncoderContext = $crate::handleResult!(vencoderctx.try_into());
-
-        if obj_raw.is_null() {
-            error!(target: log_target!(), "No provider-native object passed to encoder");
-            return ERROR_RET;
-        }
-
-        let keypair: &KeyPair = $crate::handleResult!(obj_raw.try_into());
-        match &keypair.public {
-            Some(key) => {
-                let key_bytes = key.encode();
-                use $crate::adapters::common::helpers::format_hex_bytes;
-                let formatted_key_bytes = format_hex_bytes(15, 4, &key_bytes);
-                let output = format!("Public key bytes:\n{}\n", formatted_key_bytes);
-                let output = handleResult!(CString::new(output));
-                match encoderctx.provctx.BIO_write_ex(out, &output.into_bytes_with_nul()) {
-                    Ok(_bytes_written) => {}
-                    Err(e) => {
-                        error!(target: log_target!(), "Failure using BIO_write_ex() upcall pointer: {e:?}");
-                        return ERROR_RET;
-                    }
-                };
-                return SUCCESS;
-            }
-            None => {
-                error!(target: log_target!(), "No public key");
-                return ERROR_RET;
-            }
-        }
-    }
 
     impl $crate::forge::operations::transcoders::DoesSelection for $encoder_struct {
         const SELECTION_MASK: $crate::forge::operations::keymgmt::selection::Selection =
             $crate::forge::operations::keymgmt::selection::Selection::PUBLIC_KEY;
     }
 
-    $crate::forge::operations::transcoders::make_does_selection_fn!(
-        does_selection_text,
-        $encoder_struct,
-        ProviderInstance
-    );
 
     }
 }
